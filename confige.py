cef5aabea28058fab5c058140f0dbd1c7db7df5e
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2024-06-12 10:24:40 +0330

    create some methods

--- confige.py
+++ confige.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 
 jwt = JWTManager()
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "supersecretkey"
