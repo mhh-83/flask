47c7674e60a21370c4d99dddb5a8786c47480703
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2024-06-16 18:43:46 +0330

    update home page

--- confige.py
+++ confige.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 jwt = JWTManager()
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://pachim:haghshenas67@185.79.98.202/data"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "supersecretkey"
 app.config["UPLOAD_FOLDER"] = "static/files"
