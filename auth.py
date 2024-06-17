10fd2b9fc22f36078987c64ee57ee4d3e48982bf
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2024-06-17 09:22:46 +0330

    update auth

--- auth.py
+++ auth.py
@@ -116,7 +116,7 @@ def login_user():
             jsonify(
                 {
                     "message": "Logged In ",
-                    "tokens": {"access": access_token, "refresh": refresh_token, "id":current_user.id},
+                    "tokens": {"access": access_token, "refresh": refresh_token, "id":user.id},
                 }
             ),
             200,
