c157d68c61c30b19288143b508df7b904f285046
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2024-07-11 10:03:18 +0330

    update config

--- confige.py
+++ confige.py
@@ -1,26 +1,15 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
-from sshtunnel import SSHTunnelForwarder
+
 
 jwt = JWTManager()
 app = Flask(__name__)
 
 
 
-forwarding_server = SSHTunnelForwarder(
-    ('185.79.98.202', 22),
-    ssh_username="pachim",
-    ssh_password="haghshenas67",
-    ssh_pkey="4dLW3agKOOTnhtvkgNSp4o5X/gkL+Ni6eVNoLa2Xy/A",
-    ssh_private_key_password="haghshenas67",
-    remote_bind_address=('127.0.0.1', 3306)
-)
-forwarding_server.daemon_forward_servers = True
-forwarding_server.start()
-local_port = forwarding_server.local_bind_port
 # mysql+ssh://pachim@185.79.98.202/pachim@127.0.0.1/data?name=lovely-nilofr&usePrivateKey=true
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://pachim:haghshenas67@127.0.0.1:{}/data'.format(local_port)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql:///data'
 db = SQLAlchemy(app)
 
 
