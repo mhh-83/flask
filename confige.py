65cf3665523440f96f945ad078ad7c0bb11ebdc9
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2024-07-11 11:16:41 +0330

    update config

--- confige.py
+++ confige.py
@@ -7,16 +7,16 @@ jwt = JWTManager()
 app = Flask(__name__)
 
 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "supersecretkey"
+app.config["UPLOAD_FOLDER"] = "static/files"
+
 
 # mysql+ssh://pachim@185.79.98.202/pachim@127.0.0.1/data?name=lovely-nilofr&usePrivateKey=true
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://pachim:haghshenas67@localhost/data'
 db = SQLAlchemy(app)
 
 
-
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"] = "supersecretkey"
-app.config["UPLOAD_FOLDER"] = "static/files"
 
 
 
